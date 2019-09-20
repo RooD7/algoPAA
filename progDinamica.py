62e5bbd3df066efc81585a36bf04d466112ae75d
RooD7 <rodrigosoualves7@gmail.com>
2019-09-20 15:08:16 -0300

    Programacao Dinamica

---
+++ progDinamica.py
@@ -0,0 +1,27 @@
+# Word Break Problem: Given a string and a dictionary of words,
+# determine if string can be segmented into a space-separeted 
+# sequence of one or more dictionary words.
+#
+# input
+# 	dict[] = { "this", "th", "is", "famous", "Word", "break", "b",
+#   	    "r", "e", "a", "k", "br", "bre", "brea", "ak", "problem" };
+#
+# 	string = "Wordbreakproblem"
+#
+# output
+#
+# Word b r e a k problem
+# Word b r e ak problem
+# Word br e a k problem
+# Word br e ak problem
+# Word bre a k problem
+# Word bre ak problem
+# Word brea k problem
+# Word break problem
+
+
+def main():
+	pass
+
+
+dict keys = {"this":"", "":"", "":"", "":"", "":"", "":"", "":"", "":"", "":"",}
